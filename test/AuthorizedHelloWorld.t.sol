fafa20ec17d0fa4dc43f65231d1a2d889eda73fc
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-11 09:39:13 -0500

    Update AuthorizedHelloWorld.t.sol

--- test/AuthorizedHelloWorld.t.sol
+++ test/AuthorizedHelloWorld.t.sol
@@ -1,1 +1,23 @@
-d
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "../src/AuthorizedHelloworld.sol";
+
+contract AuthorizedHelloworldtest is Test{
+    AuthorizedHelloworld hello;
+
+    function setUp() public {
+        hello = new AuthorizedHelloworld(address(1111));
+    }
+   
+    function testFailHello() public {
+        assertEq(keccak256(abi.encodePacked(hello.greeting())),  keccak256(abi.encodePacked("hello")));
+    }
+
+    function testSuccessHello() public {
+        vm.prank(address(1111));
+        assertEq(keccak256(abi.encodePacked(hello.greeting())),  keccak256(abi.encodePacked("hello")));
+    }
+
+}
+
