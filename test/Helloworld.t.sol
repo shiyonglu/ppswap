441fb2ae75f05c69e20ec35fd0ca98ef8006fbd9
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-11 09:12:39 -0500

    Update Helloworld.t.sol

--- test/Helloworld.t.sol
+++ test/Helloworld.t.sol
@@ -1,1 +1,18 @@
-sdfa
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "../src/Helloworld.sol";
+
+contract Helloworldtest is Test{
+    Helloworld hello;
+
+    function setUp() public {
+        hello = new Helloworld(address(1111));
+    }
+   
+    function testHello() public {
+        assertEq(keccak256(abi.encodePacked(hello.greeting())),  keccak256(abi.encodePacked("hello")));
+    }
+
+
+}
