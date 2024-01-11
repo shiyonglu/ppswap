0135b5f6f849c145e88132587d5e16c0392be1bf
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-11 09:58:35 -0500

    Update SafeMath.sol

--- src/SafeMath.sol
+++ src/SafeMath.sol
@@ -1,1 +1,24 @@
-d
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
+
+contract SafeMath {
+    function safeAdd(uint256 a, uint256 b) public pure returns (uint256 c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint256 a, uint256 b) public pure returns (uint256 c) {
+        require(b <= a); 
+        c = a - b; 
+    } 
+        
+    function safeMul(uint256 a, uint256 b) public pure returns (uint256 c) { 
+        c = a * b; 
+        require(a == 0 || c / a == b); 
+    } 
+        
+    function safeDiv(uint256 a, uint256 b) public pure returns (uint256 c) { 
+        require(b > 0);
+        c = a / b;
+    }
+}
+
