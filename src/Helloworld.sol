f96d2de04bb0a5bb5c645fd728c392b7cba24db9
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-11 09:05:53 -0500

    Update Helloworld.sol

--- src/Helloworld.sol
+++ src/Helloworld.sol
@@ -1,2 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
 
-asasdf
+contract Helloworld {
+    address owner;
+
+    constructor(address initialOwner){
+        owner = initialOwner;
+    }
+
+    function greeting() public returns (string memory message) {
+        return "hello";
+    }
+
+}
