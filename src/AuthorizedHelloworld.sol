087145706600911d8fac1870d7df75661ad10af5
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-11 09:37:50 -0500

    Update AuthorizedHelloworld.sol

--- src/AuthorizedHelloworld.sol
+++ src/AuthorizedHelloworld.sol
@@ -1,1 +1,21 @@
-d
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
+
+contract AuthorizedHelloworld {
+    address owner;
+
+    constructor(address initialOwner){
+        owner = initialOwner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function greeting() onlyOwner public returns (string memory message) {
+        return "hello";
+    }
+
+}
+
